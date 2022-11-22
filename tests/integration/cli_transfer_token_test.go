ed1180eca7a0f55894af09ab3f2bc304aa4c6378
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2022-11-23 02:10:00 +0500

    Add integration test by transfer token

--- tests/integration/cli_transfer_token_test.go
+++ tests/integration/cli_transfer_token_test.go
@@ -19,9 +19,23 @@ var _ = Describe("cheqd cli - positive transfert token", func() {
 		receiverAccountBalance := balance.Balances[0].Amount.Int64()
 
 		AddReportEntry("Integration", fmt.Sprintf("%sPositive: %s", cli.GREEN, "can transfer token"))
-		// Transfer token
+		// Transfer token using sender account name
 		var amount int64 = 100
 		res, err := cli.TransferToken(testdata.BASE_ACCOUNT_1_ADDRESS, testdata.BASE_ACCOUNT_2_ADDRESS, fmt.Sprintf("%dncheq", amount))
+		Expect(err).To(BeNil())
+		Expect(res.Code).To(BeEquivalentTo(0))
+
+		AddReportEntry("Integration", fmt.Sprintf("%sPositive: %s", cli.GREEN, "can query account balance"))
+		// Query the receiver account balance
+		balance, err = cli.QueryGetBalances(testdata.BASE_ACCOUNT_2_ADDRESS)
+		Expect(err).To(BeNil())
+		Expect(balance.Balances[0].Amount.Int64()).To(BeEquivalentTo(receiverAccountBalance + amount))
+
+		receiverAccountBalance = balance.Balances[0].Amount.Int64()
+
+		AddReportEntry("Integration", fmt.Sprintf("%sPositive: %s", cli.GREEN, "can transfer token"))
+		// Transfer token using sender account address
+		res, err = cli.TransferToken(testdata.BASE_ACCOUNT_1_ADDRESS, testdata.BASE_ACCOUNT_2_ADDRESS, fmt.Sprintf("%dncheq", amount))
 		Expect(err).To(BeNil())
 		Expect(res.Code).To(BeEquivalentTo(0))
 
