13321db9df9394d73bbfeaa16532f4a081b63a62
abdulla-ashurov <abdulla.ashurov@dsr-corporation.com>
2022-11-23 01:36:47 +0500

    Add 2 const variables for using in integration tests

--- tests/integration/testdata/accounts.go
+++ tests/integration/testdata/accounts.go
@@ -7,4 +7,6 @@ const (
 	CONTINUOUS_VESTING_ACCOUNT = "continuous_vesting_account"
 	DELAYED_VESTING_ACCOUNT    = "delayed_vesting_account"
 	PERIODIC_VESTING_ACCOUNT   = "periodic_vesting_account"
+	BASE_ACCOUNT_1_ADDRESS     = "cheqd1rnr5jrt4exl0samwj0yegv99jeskl0hsxmcz96"
+	BASE_ACCOUNT_2_ADDRESS     = "cheqd1l9sq0se0jd3vklyrrtjchx4ua47awug5vsyeeh"
 )
